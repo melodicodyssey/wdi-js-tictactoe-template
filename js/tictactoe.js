2794e4df67385778ef0324cb13ec6b914baf77a9
melodicodyssey <stevedbauer@gmail.com>
2014-03-13 18:23:01 -0700

    added winning combos to tictactoe.js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -28,7 +28,7 @@ $(function() {
   //### There are eight winning combos, the first two are supplied.
   //### What are the other six? Add 'em.
   var win_combos = [
-    [0,1,2], [3,4,5]
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
   ];
 
   var initialize = function() {
